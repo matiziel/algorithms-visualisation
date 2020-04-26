66c5d5c9a9642b30fe24e8af69ee324d59c8e511
Mateusz <matiziel10@gmail.com>
2020-04-26 02:09:59 +0200

    Add Node implementation in AVL tree

---
+++ AlgorithmsVisualisation/DataStructures/AVLTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class AVLTree<T> : ICollection<T> 
+        where T : IComparable<T> 
+    {
+        public AVLTree()
+        {
+            root = null;
+            Count = 0;
+        }
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Node root;
+
+        #region NodeImplementation
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node Parent { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int balance { get; set; }
+
+            public Node(T value)
+            {
+                Parent = Left = Right = null;
+                Value = value;
+                balance = 0;
+            }
+            public Node(Node parent, T value)
+            {
+                Parent = parent;
+                Left = Right = null;
+                Value = value;
+                balance = 0;
+            }
+            public bool HasChildren() => Right != null || Left != null;
+
+        }
+        #endregion
+    }
+}
