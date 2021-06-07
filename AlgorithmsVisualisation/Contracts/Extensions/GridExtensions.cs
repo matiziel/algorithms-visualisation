457c0c5b6194631a9ba352f41416db06f9bc5cad
matiziel <matiziel10@gmail.com>
2021-06-07 02:11:10 +0200

    Finish graph builder service

--- AlgorithmsVisualisation/Contracts/Extensions/GridExtensions.cs
+++ AlgorithmsVisualisation/Contracts/Extensions/GridExtensions.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 using Contracts.DataTransferObjects;
+using System;
+using Common;
 
 namespace Contracts.Extensions {
     public static class GridExtensions {
-        public static Dictionary<int, double> GetVertexEdges(this Grid grid, int x, int y) {
-            Dictionary<int, double> edges = new();
-            int index = x * y + y;
-            return edges;
-        }
+
+
+
     }
 }
