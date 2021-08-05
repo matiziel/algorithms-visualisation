c70ad3372c3d16762621cb03442b019bc25d7bfa
matiziel <matiziel10@gmail.com>
2021-08-06 01:57:13 +0200

    Remove unnecessary functions

--- AlgorithmsVisualisation/client-app/src/Utils.js
+++ AlgorithmsVisualisation/client-app/src/Utils.js
@@ -3,37 +3,6 @@ import VertexState from "./animation/VertexState";
 const Utils = {
     range: (min, max) => Array.from({ length: max - min }, (_, i) => min + i),
 
-    getGrid: (width, heigth) => {
-        let grid = Array(width);
-        for (let i = 0; i < width; ++i) {
-            grid[i] = Array(heigth);
-            for (let k = 0; k < heigth; ++k) {
-                if (i === 5 && k === 16)
-                    grid[i][k] = VertexState.Begin;
-                else if (i === 19 && k === 16)
-                    grid[i][k] = VertexState.End;
-                else
-                    grid[i][k] = VertexState.Blank;
-            }
-
-        }
-        return grid;
-    },
-
-    getRandomFrames: (amount, width, height) => {
-        let frames = [];
-
-        for (let i = 0; i < amount; ++i) {
-            let numberOfChangedVertices = Math.floor(Math.random() * 11);
-            let frame = [];
-            for (let k = 0; k < numberOfChangedVertices; ++k) {
-                frame.push([Math.floor(Math.random() * width), Math.floor(Math.random() * height), 3]);
-            }
-            frames.push(frame);
-        }
-        return frames;
-
-    },
     sleep: (ms) => {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
